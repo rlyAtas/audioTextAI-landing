c165a0910aeb022995cf88d466648be0d33448ae
Stanislav Shevchenko <stas.s.shevchenko@gmail.com>
2025-06-08 10:59:57 +0200

    check dev in config

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,14 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
+const isDev = process.env.NODE_ENV === 'development';
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
+  },
+  server: {
+    allowedHosts: isDev ? true : ['audiotextai.site'],
   }
 });
